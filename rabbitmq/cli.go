6ddb7d56d8e87e027ed056cdb2474e45ac2e4804
Nemo <guanwenbostar@163.com>
2020-08-17 23:51:12 +0800

    rabbitmq cli

---
+++ rabbitmq/cli.go
@@ -0,0 +1,91 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+
+	"github.com/streadway/amqp"
+)
+
+type RabbitOption func(op *option)
+
+type option struct {
+	Addr     string
+	Name     string
+	Exchange string
+}
+
+type Client struct {
+	option
+	conn    *amqp.Connection
+	channel *amqp.Channel
+}
+
+func NewClient(ops ...RabbitOption) (*Client, error) {
+	opt := option{}
+
+	for _, op := range ops {
+		op(&opt)
+	}
+
+	conn, err := amqp.Dial(opt.Addr)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	q, err := ch.QueueDeclare("", false, true, false, false, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	opt.Name = q.Name
+
+	res := &Client{option: opt, conn: conn, channel: ch}
+
+	return res, nil
+}
+
+func (c *Client) Bind(exchange string) error {
+	err := c.channel.QueueBind(c.Name, "", exchange, false, nil)
+	c.Exchange = exchange
+
+	return err
+}
+
+func (c *Client) Send(queue string, body interface{}) error {
+	str, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	msg := amqp.Publishing{ReplyTo: c.Name, Body: str}
+	err = c.channel.Publish("", queue, false, false, msg)
+
+	return err
+}
+
+func (c *Client) Publish(exchange string, body interface{}) error {
+	str, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	msg := amqp.Publishing{ReplyTo: c.Name, Body: str}
+	err = c.channel.Publish(exchange, "", false, false, msg)
+
+	return err
+}
+
+func (c *Client) Consume() (<-chan amqp.Delivery, error) {
+	ch, err := c.channel.Consume(c.Name, "", true, false, false, false, nil)
+
+	return ch, err
+}
+
+func (c *Client) Close() {
+	c.conn.Channel()
+}
---
+++ rabbitmq/go.mod
@@ -0,0 +1,5 @@
+module github.com/eggz6/common/rabbitmq
+
+go 1.14
+
+require github.com/streadway/amqp v1.0.0
