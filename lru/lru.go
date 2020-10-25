532850f700bc7bd5855a22a1edf78377427a851a
Nemo <guanwenbostar@163.com>
2020-10-25 17:01:49 +0800

    feat: 完成开发与单元测试

--- lru/lru.go
+++ lru/lru.go
@@ -1,6 +1,9 @@
 package lru
 
-import "container/list"
+import (
+	"container/list"
+	"sync"
+)
 
 type Cache interface {
 	Get(key string) (interface{}, bool)
@@ -8,26 +11,84 @@ type Cache interface {
 	Clear()
 }
 
+type pair struct {
+	key string
+	val interface{}
+}
+
 type cache struct {
 	size int
 	hash map[string]*list.Element
 	list *list.List
+	pool sync.Pool
 }
 
 func NewCache(size uint) Cache {
-	return &cache{
+	return newcache(size)
+}
+
+func newcache(size uint) *cache {
+	res := &cache{
 		size: int(size),
 		hash: make(map[string]*list.Element, size),
 		list: list.New(),
 	}
+
+	res.pool.New = func() interface{} {
+		return &pair{}
+	}
+
+	return res
 }
 
 func (c *cache) Get(key string) (interface{}, bool) {
+	v, ok := c.hash[key]
+	if !ok {
+		return nil, ok
+	}
 
-	return nil, false
+	c.list.MoveToBack(v)
+
+	p, _ := v.Value.(*pair)
+
+	return p.val, ok
+}
+
+func (c *cache) allocPair(key string, val interface{}) *pair {
+	obj := c.pool.Get()
+	kv, _ := obj.(*pair)
+	kv.key = key
+	kv.val = val
+
+	return kv
 }
 
 func (c *cache) Put(key string, val interface{}) {
+	v, ok := c.hash[key]
+	if !ok {
+		var kv *pair
+		if c.list.Len() >= c.size {
+			obj := c.list.Remove(c.list.Front())
+			kv, _ = obj.(*pair)
+			delete(c.hash, kv.key)
+			kv.key = ""
+			kv.val = nil
+		}
+
+		if kv == nil {
+			kv = c.allocPair(key, val)
+		}
+
+		ele := c.list.PushBack(kv)
+		c.hash[key] = ele
+
+		return
+	}
+
+	tmp := v.Value
+	kv, _ := tmp.(*pair)
+	kv.val = val
+
 	return
 }
 
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -17,6 +17,30 @@ func Test_Get(t *testing.T) {
 	}
 }
 
+func Test_Set(t *testing.T) {
+	c := newcache(1)
+	c.Put("a", 1)
+	v, ok := c.hash["a"]
+	if !ok {
+		t.Fatal("put not ok")
+	}
+
+	if p, _ := v.Value.(*pair); p.val.(int) != 1 {
+		t.Fatalf("put 1 not ok. expect=%v, actual=%v", 1, v.Value)
+	}
+
+	c.Put("b", 1)
+	_, ok = c.hash["b"]
+	if !ok {
+		t.Fatal("put b not ok")
+	}
+
+	_, ok = c.hash["a"]
+	if ok {
+		t.Fatal("put out size not ok")
+	}
+}
+
 func Test_SetGet(t *testing.T) {
 	c := NewCache(10)
 	c.Put("a", 1)
@@ -27,6 +51,40 @@ func Test_SetGet(t *testing.T) {
 
 	v, _ := val.(int)
 	if v != 1 {
-		t.Fatal("set get failed. val invalid. expect=%v, actual=%v ", 1, v)
+		t.Fatalf("set get failed. val invalid. expect=%v, actual=%v ", 1, v)
+	}
+
+	c.Put("a", 2)
+	val, ok = c.Get("a")
+	if !ok {
+		t.Fatal("set get 2 failed. not ok ")
+	}
+
+	v, _ = val.(int)
+	if v != 2 {
+		t.Fatalf("set get 2 failed. val invalid. expect=%v, actual=%v ", 2, v)
+	}
+}
+
+func Test_Recent(t *testing.T) {
+	c := NewCache(3)
+	c.Put("a", 1)
+	c.Put("b", 1)
+	c.Put("c", 1)
+	_, ok := c.Get("a")
+	if !ok {
+		t.Fatal("get recent failed. get a")
+	}
+
+	c.Put("d", 1)
+
+	_, ok = c.Get("b")
+	if ok {
+		t.Fatalf("get recent failed. expect=%v, atrual=%v", false, ok)
+	}
+
+	_, ok = c.Get("c")
+	if !ok {
+		t.Fatal("get recent failed. get c")
 	}
 }
